27e9e57634984b875ea6ba5c0f014eefa26762f2
Timini51 <129660562+Timini51@users.noreply.github.com>
2025-02-25 15:29:22 +0100

    Update coffee.js

--- coffee.js
+++ coffee.js
@@ -1,23 +1,3 @@
-const password = document.querySelector('#password');
-const button = document.querySelector('button');
-const text = document.querySelector('#text')
-let word;
-let minLength = 6;
-
-button.onclick = () => {
-    word = password.textContent.length;
-    if (word === '')
-        text.textContent = 'Enter a valid Password';
-    else if (word <= minLength )
-        text.textContent = "Password is too short";
-    else{window.alert('Welcome to CoffeeBar')};
-}
-
-const head = document.querySelector('.head');
-window.onscroll = () => {
-    head.classList('active')
-}
-
 const menu = document.querySelector('.bx-menu')
 const nav = document.querySelector('.nav')
 
